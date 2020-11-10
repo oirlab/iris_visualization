7840254ac3f02bc5ce8ede239299aa3c87c7bd77
Andrea Zonca <code@andreazonca.com>
2020-11-09 19:16:47 -0800

    fix: backend was not used

--- launch_2d_imexam_ds9.py
+++ launch_2d_imexam_ds9.py
@@ -5,6 +5,6 @@ backend = "ds9"
 # Run in the browser with Ginga
 # backend = "ginga"
 os.environ["XPA_METHOD"] = "local"
-viewer = imexam.connect(viewer="ginga")
+viewer = imexam.connect(viewer=backend)
 viewer.load_fits("test_iris_subtract_bg_flat_cal.fits")
 viewer.scale()
